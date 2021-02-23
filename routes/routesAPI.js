35c3d7f14d2004197abb72c746d6f750f5e1b89a
jlw429 <jlw00329@gmail.com>
2021-02-22 23:49:07 -0500

    server will not work properly

--- routes/routesAPI.js
+++ routes/routesAPI.js
@@ -1,21 +1,49 @@
-const jsonDB = require('../db/db.json');
+const jsonDB = require('../routes/db.json');
+const fs = require('fs');
+const util = require('util');
 
+// Handling Asynchronous Processes
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
 
 module.exports = (app) => {
-  
-    app.get('/api/notes', (req, res) => {
-        res.json(jsonDB)
-    })
-    
-    app.post('/api/notes', (req, res) => {
-        jsonDB.push(req.body);
-        res.json(true);
-        })
+  app.get('/api/notes', function (req, res) {
+    readFileAsync('../routes/db.json', 'utf8').then(function (data) {
+      notes = [].concat(JSON.parse(data));
+      res.json(notes);
+    });
+  });
 
-    app.delete('/api/notes/:id', (req, res) => {
-        const id = req.params.id;
-        jsonDB.splice(id, 1);
-        res.json(jsonDB)
-    })
-   
+  app.post('/api/notes', function (req, res) {
+    const note = req.body;
+    readFileAsync('../routes/db.json', 'utf8')
+      .then(function (data) {
+        const notes = [].concat(JSON.parse(data));
+        note.id = notes.length + 1;
+        notes.push(note);
+        return notes;
+      })
+      .then(function (notes) {
+        writeFileAsync('../routes/db.json', JSON.stringify(notes));
+        res.json(note);
+      });
+  });
+  app.delete('/api/notes/:id', function (req, res) {
+    const idToDelete = parseInt(req.params.id);
+    readFileAsync('../routes/db.json', 'utf8')
+      .then(function (data) {
+        const notes = [].concat(JSON.parse(data));
+        const newNotesData = [];
+        for (let i = 0; i < notes.length; i++) {
+          if (idToDelete !== notes[i].id) {
+            newNotesData.push(notes[i]);
+          }
+        }
+        return newNotesData;
+      })
+      .then(function (notes) {
+        writeFileAsync('../routes/db.json', JSON.stringify(notes));
+        res.send('saved success!!!');
+      });
+  });
 };
--- server.js
+++ server.js
@@ -1,21 +1,22 @@
 // DEPENDENCIES
 const express = require('express');
-const app = express();
 const htmlRoute = require('./routes/routesHtml');
 
 // Sets initial port
+const app = express();
 const PORT = process.env.PORT || 8080;
 
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
-app.use(htmlRoute);
 
+//middleware
+app.use(express.static("public"));
 
 //route locations
+app.use(htmlRoute);
 require('./routes/routesAPI')(app);
-// require('./Develop/routes/routesHtml')(app);
+
 
 app.listen(PORT, () => {
   console.log(`App listening on PORT: ${PORT}`);
